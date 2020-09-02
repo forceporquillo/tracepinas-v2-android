9ec0023ed1bd99a5c02506fd675f982744cf1871
Force Porquillo <fporquillo18@gmail.com>
2020-09-03 03:18:56 +0800

    Add build debug tree components

---
+++ app/src/main/java/com/force/codes/tracepinas/util/debug/DetectLeak.kt
@@ -0,0 +1,31 @@
+/*
+ * Created by Force Porquillo on 9/2/20 4:43 AM
+ */
+
+package com.force.codes.tracepinas.util.debug
+
+import android.os.StrictMode
+import leakcanary.AppWatcher
+
+object DetectLeak{
+  @JvmStatic fun startLeak() {
+    AppWatcher.config.watchActivities
+    AppWatcher.config.watchFragments
+    AppWatcher.config.watchFragmentViews
+    AppWatcher.config.watchViewModels
+
+    val objectWatcher =
+      AppWatcher.objectWatcher
+    objectWatcher.retainedObjectCount
+  }
+
+  @JvmStatic fun enabledStrictMode() {
+    StrictMode.setThreadPolicy(
+        StrictMode.ThreadPolicy.Builder()
+            .detectAll()
+            .penaltyLog()
+            .penaltyDeath()
+            .build()
+    )
+  }
+}
