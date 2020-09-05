754d7dde4730a01369465d574b13bd9d0778efdc
Force Porquillo <fporquillo18@gmail.com>
2020-09-05 12:28:47 +0800

    Add ThreadExecutor dependency instance

--- app/src/main/java/com/force/codes/tracepinas/di/module/AppModule.kt
+++ app/src/main/java/com/force/codes/tracepinas/di/module/AppModule.kt
@@ -4,13 +4,28 @@
 
 package com.force.codes.tracepinas.di.module
 
+import android.app.Application
 import com.force.codes.tracepinas.BaseApplication
+import com.force.codes.tracepinas.util.service.ThreadExecutor
 import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+private const val DELAY_QUEUE = 0
 
 @Module
 class AppModule constructor(
-    private val _application: BaseApplication
+  private val _application: BaseApplication,
 ) {
-    val getApplication : BaseApplication
-    get() = _application
+
+  @Provides @Singleton
+  fun providesApplication(): Application {
+    return _application
+  }
+
+  @Provides
+  @Singleton
+  fun providesExecutor(): ThreadExecutor {
+    return ThreadExecutor(DELAY_QUEUE)
+  }
 }
