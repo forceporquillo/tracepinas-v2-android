dc8f10aa912a297b7d05b79c26114f4b92f88a8c
Force Porquillo <fporquillo18@gmail.com>
2020-09-05 12:26:08 +0800

    Add DOH data drop model

---
+++ app/src/main/java/com/force/codes/tracepinas/data/model/doh_csv_data_drop/DataDrop.kt
@@ -0,0 +1,64 @@
+/*
+ * Created by Force Porquillo on 9/4/20 5:25 PM
+ */
+
+package com.force.codes.tracepinas.data.model.doh_csv_data_drop
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+/**
+ * DOH data drop from csv
+ */
+
+@Entity
+data class DataTimeline(
+  @PrimaryKey
+  val id: Int = 1,
+
+  @Embedded
+  @field:SerializedName("data")
+  val data: List<DayOne>
+)
+
+@Entity
+data class DayOne(
+  @PrimaryKey
+  val uid: Int,
+
+  @field:SerializedName("date")
+  val date: String? = null,
+
+  @field:SerializedName("recovered")
+  val recovered: Int = 0,
+
+  @field:SerializedName("cases")
+  val cases: Int = 0,
+
+  @field:SerializedName("died")
+  val died: Int = 0,
+)
+
+data class Regions(
+  @field:SerializedName("data")
+  val data: List<Provinces?>? = null,
+)
+
+@Entity
+data class Provinces(
+  @PrimaryKey
+  @field:SerializedName("region")
+  val region: String,
+
+  @field:SerializedName("recovered")
+  val recovered: Int = 0,
+
+  @field:SerializedName("cases")
+  val cases: Int = 0,
+
+  @field:SerializedName("deaths")
+  val deaths: Int = 0,
+)
+
