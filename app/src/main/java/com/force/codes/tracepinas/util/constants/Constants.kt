3ddbe975279e9f534990896e8ead927b8b2d2243
strongforce1 <201811941@fit.edu.ph>
2020-09-08 23:58:55 +0800

    Add Constants

---
+++ app/src/main/java/com/force/codes/tracepinas/util/constants/Constants.kt
@@ -0,0 +1,14 @@
+/*
+ * Created by Force Porquillo on 9/4/20 6:31 PM
+ */
+
+package com.force.codes.tracepinas.util.constants
+
+const val TIMEOUT_MILLIS = 1000
+const val START_DATE = "2020-01-22"
+const val TIME_FORMAT = "yyyy-MM-dd"
+
+const val URL_PATH = "https://corona.lmao.ninja/v2/"
+
+const val TWITTER_BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAA5SDwEAAAAAnF1q%2BmhNSAysxKDen" +
+    "HX4De2H7x0%3DyUJnEvO0oet43pc2mWNGTlNFyF6kXOeprKwF2wx5RRpixV3WBu"
