7f8f0245444ae39210ecf27c1b7152e7facc6862
strongforce1 <201811941@fit.edu.ph>
2020-09-09 13:14:37 +0800

    Update DataStoreUtil.kt

--- app/src/main/java/com/force/codes/tracepinas/util/DataStoreUtil.kt
+++ app/src/main/java/com/force/codes/tracepinas/util/DataStoreUtil.kt
@@ -10,28 +10,30 @@ import androidx.datastore.preferences.Preferences
 import androidx.datastore.preferences.createDataStore
 import androidx.datastore.preferences.edit
 import androidx.datastore.preferences.preferencesKey
+import com.force.codes.tracepinas.util.constants.DEFAULT
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 private const val PREF_INT_KEY = "FIRST_INSTALL_KEY"
 private const val PREF_PRIMARY_KEY = "PRIMARY_COUNTRY_KEY"
+private const val DATA_STORE = "DATA_PREF"
 
 open class DataStoreUtil(
   private val context: Context,
 ) {
 
   private val dataStore: DataStore<Preferences> by lazy {
-    context.createDataStore(name = "data_pref")
+    context.createDataStore(name = DATA_STORE)
   }
 
   companion object {
-    val COUNTRY_PREF_KEY = preferencesKey<Int>(PREF_INT_KEY)
-    val PRIMARY_PREF = preferencesKey<String>(PREF_PRIMARY_KEY)
+    val COUNTRY_PREF_KEY by lazy { preferencesKey<Int>(PREF_INT_KEY) }
+    val PRIMARY_PREF by lazy { preferencesKey<String>(PREF_PRIMARY_KEY) }
   }
 
   val getStoredVersionCode: Flow<Int> = dataStore.data
-      .map { preference ->
-        preference[COUNTRY_PREF_KEY] ?: -1
+      .map { preferences ->
+        preferences[COUNTRY_PREF_KEY] ?: -1
       }
 
   suspend fun storeVersionCode(
@@ -44,7 +46,7 @@ open class DataStoreUtil(
 
   val getStoredCountry: Flow<String?> = dataStore.data
       .map { countryKey ->
-        countryKey[PRIMARY_PREF] ?: "Philippines"
+        countryKey[PRIMARY_PREF] ?: DEFAULT
       }
 
   suspend fun storePrimaryCountry(
