cd066699e94a7f8eaad930fae807c8a0115e624b
strongforce1 <201811941@fit.edu.ph>
2020-09-11 17:18:03 +0800

    Create Constants.kt

---
+++ app/src/main/java/com/force/codes/tracepinas/constants/Constants.kt
@@ -0,0 +1,34 @@
+/*
+ * Created by Force Porquillo on 9/4/20 6:31 PM
+ */
+
+package com.force.codes.tracepinas.constants
+
+
+object Constants {
+  const val TIMEOUT_MILLIS = 1000
+
+  const val START_DATE = "2020-01-22"
+
+  const val TIME_FORMAT = "yyyy-MM-dd"
+
+  const val DEFAULT = "Philippines"
+
+  private const val URL_PATH = "https://corona.lmao.ninja/v2/"
+
+  const val TWITTER_BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAA5SDwEAAAAAnF1q%2BmhNSAysxKDen" +
+      "HX4De2H7x0%3DyUJnEvO0oet43pc2mWNGTlNFyF6kXOeprKwF2wx5RRpixV3WBu"
+
+  fun basePath(endpoint: String?): String {
+    return URL_PATH.plus(endpoint)
+  }
+}
+
+object PageListConstants {
+  const val PAGE_SIZE = 10 // page size
+  const val PRE_FETCH_DISTANCE = 20
+  const val PAGE_INITIAL_LOAD_SIZE_HINT = 30 // page size on first load
+  const val PAGE_MAX_SIZE = 215 // triggers when to fetch a page
+  const val isEnable = false
+}
+
