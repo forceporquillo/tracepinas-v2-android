b4bedd57968a79b6813540391b583190cd6425ff
strongforce1 <201811941@fit.edu.ph>
2020-09-11 17:21:28 +0800

    Update ActivityBuilderModule.kt

--- app/src/main/java/com/force/codes/tracepinas/di/module/ActivityBuilderModule.kt
+++ app/src/main/java/com/force/codes/tracepinas/di/module/ActivityBuilderModule.kt
@@ -4,12 +4,23 @@
 
 package com.force.codes.tracepinas.di.module
 
+<<<<<<< Updated upstream
 import com.force.codes.tracepinas.ui.activity.NavHostActivity
+=======
+import com.force.codes.tracepinas.ui.activity.navigation_host.NavHostActivity
+import com.force.codes.tracepinas.ui.activity.list_view.ChangeCountryActivity
+>>>>>>> Stashed changes
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class ActivityBuilderModule {
-  @ContributesAndroidInjector(modules = [FragmentBuilderModule::class])
+  @ContributesAndroidInjector(modules = [FragmentNavHostModule::class])
   abstract fun bindFragmentContainerActivity(): NavHostActivity
+<<<<<<< Updated upstream
+=======
+
+  @ContributesAndroidInjector()
+  abstract fun bindFragmentListViewActivity(): ChangeCountryActivity
+>>>>>>> Stashed changes
 }
