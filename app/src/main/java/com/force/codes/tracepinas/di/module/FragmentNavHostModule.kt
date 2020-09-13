5294b720c2737d42a35ae732c9cd81bf9c6b08b8
strongforce1 <201811941@fit.edu.ph>
2020-09-13 22:10:34 +0800

    Update fragment module to CurrentCountryFragment

--- app/src/main/java/com/force/codes/tracepinas/di/module/FragmentNavHostModule.kt
+++ app/src/main/java/com/force/codes/tracepinas/di/module/FragmentNavHostModule.kt
@@ -4,7 +4,7 @@
 
 package com.force.codes.tracepinas.di.module
 
-import com.force.codes.tracepinas.ui.fragment.viewpager.CountryFragment
+import com.force.codes.tracepinas.ui.fragment.viewpager.CurrentCountryFragment
 import com.force.codes.tracepinas.ui.fragment.viewpager.listview.ListViewFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
@@ -12,7 +12,7 @@ import dagger.android.ContributesAndroidInjector
 @Module
 abstract class FragmentNavHostModule {
   @ContributesAndroidInjector
-  abstract fun contributeCountryFragment() : CountryFragment
+  abstract fun contributeCountryFragment() : CurrentCountryFragment
 
   @ContributesAndroidInjector
   abstract fun contributeListViewFragment() : ListViewFragment
