f69cb5351f8da5fcf976c9b5d788062cb4554696
strongforce1 <201811941@fit.edu.ph>
2020-09-13 21:38:54 +0800

    Add ViewModel multibindings

---
+++ app/src/main/java/com/force/codes/tracepinas/di/factory/ViewModelProviderFactory.kt
@@ -0,0 +1,43 @@
+/*
+ * Created by Force Porquillo on 9/11/20 11:37 AM
+ */
+
+package com.force.codes.tracepinas.di.factory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Singleton
+
+private const val UNKNOWN = "Unknown model class "
+
+@Singleton
+open class ViewModelProviderFactory @Inject constructor(
+  private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
+) : ViewModelProvider.Factory {
+
+  @Suppress("UNCHECKED_CAST")
+  override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    var creator = creators[modelClass]
+
+    creator?.let {
+      for (entry in creators) {
+        if (modelClass.isAssignableFrom(entry.key)) {
+          creator = entry.value
+          break
+        }
+      }
+    }
+
+    if (creator == null) {
+      throw IllegalArgumentException(UNKNOWN.plus("$modelClass"))
+    }
+
+    return try {
+      creator!!.get() as T
+    } catch (e: Exception) {
+      throw RuntimeException(e)
+    }
+  }
+}
