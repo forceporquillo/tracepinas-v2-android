802498e3f32bc6a2fcb993babe42a00f198ff6e7
strongforce1 <201811941@fit.edu.ph>
2020-09-13 21:36:56 +0800

    Add onStartListViewActivity intent listener

---
+++ app/src/main/java/com/force/codes/tracepinas/callback/ListActivityListener.kt
@@ -0,0 +1,12 @@
+/*
+ * Created by Force Porquillo on 9/2/20 5:38 AM
+ */
+
+package com.force.codes.tracepinas.callback
+
+import android.view.View
+
+interface ListActivityListener {
+  fun onStartListViewActivity(view: View?)
+}
+
