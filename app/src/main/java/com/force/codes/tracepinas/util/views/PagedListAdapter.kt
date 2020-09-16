393b9d0784384f9304d01214fb5e4f68036f66fb
strongforce1 <201811941@fit.edu.ph>
2020-09-16 23:56:40 +0800

    Update PagedListAdapter

--- app/src/main/java/com/force/codes/tracepinas/util/views/PagedListAdapter.kt
+++ app/src/main/java/com/force/codes/tracepinas/util/views/PagedListAdapter.kt
@@ -2,57 +2,45 @@
  * Created by Force Porquillo on 9/15/20 12:49 AM
  */
 
-/*
- * Created by Force Porquillo on 9/15/20 12:34 AM
- */
-
 package com.force.codes.tracepinas.util.views
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.force.codes.tracepinas.data.entities.PerCountry
 import com.force.codes.tracepinas.util.constants.LIST_VIEW
-import com.force.codes.tracepinas.util.constants.SUPPRESS
-import com.force.codes.tracepinas.custom.ListViewHolder
 import com.force.codes.tracepinas.databinding.PerCountryListBinding
 import com.force.codes.tracepinas.util.DiffUtilComparator
 
 open class PagedListAdapter<T>(
   private val holderType: Int,
-  private val listener: GenericPagedListListener,
+  private val listener: AdapterListener,
 ) : PagedListAdapter<T, ViewHolder>(DiffUtilComparator<T>()) {
 
   override fun onCreateViewHolder(
     parent: ViewGroup, viewType: Int,
   ): ViewHolder {
     val inflater = LayoutInflater.from(parent.context)
-
+    val binding : ViewDataBinding
     when (holderType) {
-      LIST_VIEW -> {
-        val binding = PerCountryListBinding.inflate(inflater, parent, false)
-        return ListViewHolder(binding, listener)
-      }
-      else -> throw IllegalArgumentException()
+      LIST_VIEW -> binding = PerCountryListBinding.inflate(inflater, parent, false)
+      else -> throw ClassCastException()
     }
+    return ViewHolderFactory<PerCountry>(binding, listener)
   }
 
-  @Suppress(SUPPRESS)
   override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-    (holder as Binder<T>).bind(getItemPositionAt(position)!!)
-  }
-
-  @Suppress(SUPPRESS)
-  private fun <T> getItemPositionAt(position: Int): T? {
-    return getItem(position) as T?
-  }
-
-  internal interface Binder<T> {
-    fun bind(data: T)
+    (holder as Binder<*>).bind(getItem(position))
   }
 }
 
-interface GenericPagedListListener {
+internal interface Binder<T> {
+  fun bind(data: Any?)
+}
+
+interface AdapterListener {
   fun onGetAdapterPosition(position: Int)
 }
 
