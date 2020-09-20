0b1f7c9de376e0a6f584125c0ab246eb0f4558d4
strongforce1 <201811941@fit.edu.ph>
2020-09-20 15:38:54 +0800

    Update ViewExtensions.kt

--- app/src/main/java/com/force/codes/tracepinas/util/views/ViewExtensions.kt
+++ app/src/main/java/com/force/codes/tracepinas/util/views/ViewExtensions.kt
@@ -5,6 +5,7 @@
 package com.force.codes.tracepinas.util.views
 
 import android.view.View
+import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
